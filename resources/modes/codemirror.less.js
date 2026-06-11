daf47e28a7cad6f7af95e7883f763652cde286ca
TehKittyCat <tehkittycat@weirdgloop.org>
2026-06-10 20:34:06 -0400

    Drop LESS linting support for now. Would require additional work due to how stylelint is bundled.

--- resources/modes/codemirror.less.js
+++ resources/modes/codemirror.less.js
@@ -1,7 +1,6 @@
 const { syntaxTree } = require( 'ext.CodeMirror.lib' );
 const { lessLanguage, lessCompletionSource } = require( '../lib/codemirror.bundle.modes.js' );
 const CodeMirrorMode = require( './codemirror.mode.js' );
-const CodeMirrorWorker = require( '../workers/codemirror.worker.js' );
 
 /**
  * LESS language support for CodeMirror.
@@ -24,42 +23,12 @@ class CodeMirrorLess extends CodeMirrorMode {
 
 	/** @inheritDoc */
 	get lintSource() {
-		return async ( view ) => {
-			const data = await this.worker.lint( view );
-			return data.map( ( {
-				text,
-				severity,
-				line,
-				column,
-				endLine,
-				endColumn,
-				rule,
-				fix
-			} ) => {
-				const diagnostic = {
-					rule,
-					source: 'Stylelint',
-					message: text,
-					severity: severity === 'error' ? 'error' : 'info',
-					from: CodeMirrorWorker.pos( view, line, column ),
-					to: endLine === undefined ?
-						view.state.doc.line( line ).to :
-						CodeMirrorWorker.pos( view, endLine, endColumn )
-				};
-				if ( fix ) {
-					const { range: [ from, to ], text: insert } = fix;
-					diagnostic.actions = [
-						{
-							name: 'fix',
-							apply( v ) {
-								v.dispatch( { changes: { from, to, insert } } );
-							}
-						}
-					];
-				}
-				return diagnostic;
-			} );
-		};
+		return undefined;
+	}
+
+	/** @inheritDoc */
+	get hasWorker() {
+		return false;
 	}
 
 	/** @inheritDoc */
